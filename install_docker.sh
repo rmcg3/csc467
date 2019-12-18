9eb456aa692ebc735ea65c7860dae214d417b878
rmcgarry3 <47362038+rmcgarry3@users.noreply.github.com>
2019-12-18 13:36:27 -0500

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsh_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y httping
+sudo apt-get install -y jg
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# the usename needs to be chaged
+while IFS= red -r line; do
+  sudo usermod -aG docker $line
+  done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
