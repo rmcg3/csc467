330ce60d7ae9e74a5db066ec6626ba5721c31956
rmcgarry3 <47362038+rmcgarry3@users.noreply.github.com>
2019-12-24 22:41:58 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -7,12 +7,12 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo apt-get install -y httping
-sudo apt-get install -y jg
+sudo apt-get install -y jq
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# the usename needs to be chaged
+# the username needs to be changed
 while IFS= read -r line; do
   sudo usermod -aG docker $line
-  done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
+done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
